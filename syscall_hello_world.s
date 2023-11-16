2075088b8501242b8d2ec51c23cf9f72f6ab6172
William Redding <williamdredding@gmail.com>
2023-11-16 11:10:13 +0000

    Create syscall_hello_world.s

---
+++ syscall_hello_world.s
@@ -0,0 +1,19 @@
+# -nostdlib
+.global _start
+
+_start:
+    # write syscall
+    mov $1, %rax                # syscall code for write
+    mov $1, %rdi                # file descriptor for write (1 - stdout)
+    mov $hello_world, %rsi      # load effective address of null termianted string into register
+    mov $14, %rdx               # load string length
+    syscall
+    
+    # exit syscall
+    mov $60, %rax                # syscall code for exit
+    mov $0, %rdi                 # return code
+    syscall
+
+hello_world:
+    # null terminated hello world string
+    .asciz "Hello, World!\n"
