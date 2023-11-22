d15b5b040817b572428c16cf1b9e69235a764f32
Spacerulerwill <williamdredding@gmail.com>
2023-11-22 13:00:24 +0000

    Assembly comments for linear search

--- linear_search/linear_search.s
+++ linear_search/linear_search.s
@@ -7,15 +7,15 @@
 
 .global  linear_search
 linear_search:
-    xor %rax, %rax
-    jmp loop_check
-loop_body:
-    cmp (%rdi), %rdx
-    je exit
-    add $8, %rdi
-    inc %rax
+    xor %rax, %rax              # rax is the current index counter
+    jmp loop_check              # jump to first execution of the loop check
+loop_body:                      
+    cmp (%rdi), %rdx            # rsi stores the array pointer, compare value at pointer to element we are trying to locate
+    je exit                     # if they match, branch to exit and return (index stored in rax)
+    add $8, %rdi                # otherwise we go to next element (8 bytes along)
+    inc %rax                    # increase current index
 loop_check:
-    cmp %rsi, %rax
-    jl loop_body
+    cmp %rsi, %rax              # compare index and array length
+    jl loop_body                # if less than, go back to loop body
 exit:
-    ret
+    ret                         # return index (stored in rax)
